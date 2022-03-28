26b1994f64fb2098cbd2cb39b3bdd0a8223842d5
byeonghun <byeonghun08@gmail.com>
2022-03-28 22:54:21 +0900

    db 연동 env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath:
+                process.env.NODE_ENV === 'development'
+                    ? '.development.env'
+                    : '.env',
+        }),
+        MongooseModule.forRoot(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }),
+    ],
     controllers: [AppController, AuthController],
     providers: [AppService, AuthService],
 })
